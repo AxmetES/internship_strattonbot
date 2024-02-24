76dab626f19ead2fd140432d4c78a84d71619a97
axmetes <fraktsia@gmail.com>
2024-02-24 20:22:35 +0600

    Fix. Change bot answer.

--- tg_bot.py
+++ tg_bot.py
@@ -8,9 +8,8 @@ bot = telebot.TeleBot(settings.BOT_TOKEN)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    if 'Strattonbot' in message.text:
-        crud.insert_message(message.text, message.json['date'])
-        bot.send_message(message.chat.id, message.text)
+    crud.insert_message(message.text, message.json['date'])
+    bot.send_message(message.chat.id, f'Strattonbot {message.text}')
 
 
 if __name__ == "__main__":
